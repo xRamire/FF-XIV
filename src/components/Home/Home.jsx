67d503efc392a0d19d65427814f835f2d3faf9ae
Adrian Ramirez <ramiire@gmail.com>
2022-12-13 19:27:27 +0100

    changes home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import './home.css' ;
 import MainImg from '../../assets/img/Final-Fantasy-XIV-home-Logo.png';
-import Exp1 from '../../assets/img/realmreborn.png';
-import Exp2 from '../../assets/img/heavensward.png';
-import Exp3 from '../../assets/img/stormblood.png';
-import Exp4 from '../../assets/img/shadowbringers.png';
-import Exp5 from '../../assets/img/endwalker.png';
-
-
 
 function Home(props) {
     return (
         <div id="Home" className='landing'>
-            <div className="container auto">
-                <div className="content">
+            <div className="container auto bg1">
+                <div className="content padd-t-100">
                     <div class="main-img auto">
                         <img src={MainImg} alt="Final Fantasy" />
                     </div>
                     <div className='txt center'>
                         <h1>Adventure awaits you beyond the horizon</h1>
                     </div>
-                    <div className='line'></div>
-                    <div className="expansions column">
-                        <div className="row">
-                            <div className="auto exp-card margin-r-5"><img src={Exp1} alt="realm reborn" /></div>
-                            <div className="auto exp-card margin-l-5"><img src={Exp2} alt="realm reborn" /></div>
+                </div>
+
+            </div>
+
+            <div className="container auto bg2">
+                <div className="content verticalalign">
+                    <div className="expansions column verticalalign">
+                        <div className="row card-space auto padd-m-t-50">
+                            <div className=" exp-card"><div className="exp-card-img exp-card-img1"></div></div>
+                            <div className=" exp-card"><div className="exp-card-img exp-card-img2"></div></div>
                         </div>
-                        <div className="row">
-                            <div className="auto exp-card margin-r-5"><img src={Exp3} alt="realm reborn" /></div>
-                            <div className="auto exp-card margin-l-5"><img src={Exp4} alt="realm reborn" /></div>
+                        <div className="row card-space auto padd-t-50 padd-m-t-0">
+                            <div className="auto exp-card"><div className="exp-card-img exp-card-img3"></div></div>
+                            <div className="auto exp-card"><div className="exp-card-img exp-card-img4"></div></div>
                         </div>
-                        <div className="auto exp-card"><img src={Exp5} alt="realm reborn" /></div>
+                        <div className="row card-space auto padd-t-50 padd-m-t-0">
+                            <div className="auto exp-card"><div className="exp-card-img exp-card-img5"></div></div>
+                        </div>
                     </div>
                 </div>
             </div>
