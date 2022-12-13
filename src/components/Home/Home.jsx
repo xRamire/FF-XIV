e5f38d14f173fe53a5bb0eda8e7fd525619267eb
Adrian Ramirez <ramiire@gmail.com>
2022-12-14 00:01:53 +0100

    little changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './home.css' ;
+import './home.css';
 import MainImg from '../../assets/img/Final-Fantasy-XIV-home-Logo.png';
 
 function Home(props) {
@@ -12,6 +12,9 @@ function Home(props) {
                     </div>
                     <div className='txt center'>
                         <h1>Adventure awaits you beyond the horizon</h1>
+                    </div>
+                    <div class="arrow auto">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g><polygon points="13.707 4.707 12.293 3.293 3.586 12 12.293 20.707 13.707 19.293 6.414 12 13.707 4.707" /><polygon points="19.707 4.707 18.293 3.293 9.586 12 18.293 20.707 19.707 19.293 12.414 12 19.707 4.707" /></g></svg>
                     </div>
                 </div>
 
